09357b8f1cfa954828b1c0872a504808a93b1f66
kimpoy813 <kimdave.rafael@nlpsc.edu.ph>
2026-07-15 09:22:58 +0800

    Removed MOA

--- conf/settings.py
+++ conf/settings.py
@@ -44,7 +44,6 @@ INSTALLED_APPS = [
     'accounts',
     'axes', 
     'proposals',
-    'moa',
 ]
 
 MIDDLEWARE = [
--- moa/apps.py
+++
@@ -1,7 +0,0 @@
-from django.apps import AppConfig
-
-
-class MoaConfig(AppConfig):
-    default_auto_field = "django.db.models.BigAutoField"
-    name = "moa"
-    verbose_name = "MOA Phase"
